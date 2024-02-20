232b355c21468d6f4b81aa68364cb76fb5d6257d
Josphat_19 <josphatgg19@gmail.com>
2024-02-20 14:27:51 +0300

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -257,7 +257,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
+                {formData.type === 'rent' &&(
                 <span className='text-xs'>($ / night)</span>
+                )}
               </div>
             </div>  
             {formData.offer && (
@@ -273,7 +275,9 @@ export default function CreateListing() {
                />
               <div className='flex flex-col items-center'>
                 <p>Discounted price</p>
+                {formData.type === 'rent' &&(
                 <span className='text-xs'>($ / night)</span>
+                )}
               </div>
             </div>
             )}      
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -275,7 +275,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
+                {formData.type === 'rent' && (
                 <span className='text-xs'>($ / night)</span>
+                )}
               </div>
             </div>  
             {formData.offer && (
